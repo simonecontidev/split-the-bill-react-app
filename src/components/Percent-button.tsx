c49567bc0627ca44e2a6f56b33555ca434f4440b
simonecontidev <barriotropical@gmail.com>
2025-10-01 12:17:40 +0200

    custom button style
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

--- src/components/Percent-button.tsx
+++ src/components/Percent-button.tsx
@@ -4,8 +4,14 @@ interface Props{
 }
 
 export const PercentButton = ({percent, isCustom = false}:Props) => {
+
+    const styles = isCustom 
+    ? ' bg-sky-800 text-white placeholder:text-bg' 
+    : 'bg-bg text-amber-50 hover:bg-blue-300 hover:text-bg'
+    ;
+
   return (
-    <button className='text-2xl font-bold bg-bg text-amber-50 rounded-[5px] w-full h-12 cursor-pointer hover:bg-blue-300 hover:text-bg'>
+    <button className={`text-2xl font-bold rounded-[5px] w-full h-12 cursor-pointer hover:bg-blue-300 hover:text-bg ${styles}`}>
         {
             isCustom ? 'Custom' : `${percent}%`
         }
