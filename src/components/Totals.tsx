b066d3c3b5918cebedb786ad9bb750ec5ece96f4
simonecontidev <barriotropical@gmail.com>
2025-10-01 16:37:46 +0200

    print tip amount
    Signed-off-by: simonecontidev <barriotropical@gmail.com>

--- src/components/Totals.tsx
+++ src/components/Totals.tsx
@@ -1,10 +1,13 @@
 import { ButtonReset, TotalItem } from "../components"
+import { useCalculatorStore } from "../store/Calculator.store"
 
 export const Totals = () => {
+
+    const store = useCalculatorStore();
   return (
     <div className="bg-black rounded-[15px] py-6 text-white px-8 lg:w-[413px] lg:flex lg: flex-col lg:justify-between">
         <div>
-        <TotalItem text='Tip Amount' value={0}/>
+        <TotalItem text='Tip Amount' value={store.tipAmount()}/>
         <TotalItem text='Total' value={32.79} />
         </div>
         <ButtonReset text="Reset"/>
--- src/store/Calculator.store.ts
+++ src/store/Calculator.store.ts
@@ -9,28 +9,39 @@ interface Calculator {
   setBill: (newBill: number) => void;
   setPeople: (newPeople: number) => void;
   setTip: (newTip: number) => void;
+
+
+  //value calculated
+  tipAmount:()=> number;
+  total:()=> number;
 }
 
 export const useCalculatorStore = create<Calculator>()(
   devtools(
-    (set) => ({
+    (set, get) => ({
       bill: 0,
       people: 1,
       tip: 5,
 
     
       setBill: (newBill) =>
-        set({ bill: newBill }),
+      set({ bill: newBill }),
         // setBill: (newBill) =>
         // set(() => ({ bill: newBill }), false, 'calculator/setBill'),
-
 
       setPeople: (newPeople: number) =>
       set(()=>({ people: newPeople })),
 
-    setTip: (newTip: number) =>
+      setTip: (newTip: number) =>
       set(() => ({ tip: newTip })),
+
+
+
+
+tipAmount: () => (get().bill * get().tip / 100) / get().people,
     })
     
+
+
   )
 );
